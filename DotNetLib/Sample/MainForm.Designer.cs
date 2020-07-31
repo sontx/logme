6be43658ee7293cc3d0500cc71c289d088164e4c
sontx <xuanson33bk@gmail.com>
2020-07-31 16:16:49 +0700

    ! Add sample project for .net

---
+++ DotNetLib/Sample/MainForm.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace Sample
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnWriteRandomLog = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnWriteRandomLog
+            // 
+            this.btnWriteRandomLog.Location = new System.Drawing.Point(12, 12);
+            this.btnWriteRandomLog.Name = "btnWriteRandomLog";
+            this.btnWriteRandomLog.Size = new System.Drawing.Size(154, 23);
+            this.btnWriteRandomLog.TabIndex = 0;
+            this.btnWriteRandomLog.Text = "Write Random Log";
+            this.btnWriteRandomLog.UseVisualStyleBackColor = true;
+            this.btnWriteRandomLog.Click += new System.EventHandler(this.btnWriteRandomLog_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 41);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(390, 288);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.WordWrap = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 341);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnWriteRandomLog);
+            this.Name = "MainForm";
+            this.Text = "DotNetLib Sample";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnWriteRandomLog;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ DotNetLib/Sample/MainForm.cs
@@ -0,0 +1,56 @@
+﻿using LogMeLib;
+using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Sample
+{
+    public partial class MainForm : Form
+    {
+        private readonly Random random = new Random(DateTime.Now.Millisecond);
+        private LogMe logMe;
+        private int counter;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            logMe = new LogMe("ws://mqtt.eclipse.org:80/mqtt");
+        }
+
+        protected override async void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            await logMe.StartAsync();
+        }
+
+        protected override async void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            await logMe.StopAsync();
+        }
+
+        private void btnWriteRandomLog_Click(object sender, EventArgs e)
+        {
+            string msg = "";
+            switch (random.Next(3))
+            {
+                case 0:
+                    msg = "Random info message " + Interlocked.Increment(ref counter);
+                    Logger.I(msg);
+                    break;
+
+                case 1:
+                    msg = "Random debug message " + Interlocked.Increment(ref counter);
+                    Logger.D(msg);
+                    break;
+
+                case 2:
+                    msg = "Random error message " + Interlocked.Increment(ref counter);
+                    Logger.E(msg, new Exception("This is a bullshit exception"));
+                    break;
+            }
+
+            textBox1.AppendText(msg + Environment.NewLine);
+        }
+    }
+}
